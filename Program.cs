cc629a5773529b9ddefae84f3f28d87c8bd5f210
Raso221 <mrasoolrsoltani2006@gmail.com>
2024-11-12 14:14:12 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class program
+{
+    static void Main(string[]   args)
+    {
+        Console.WriteLine("please select your favourite coffee");
+        Console.WriteLine(1);
+        Console.WriteLine(2);
+        Console.WriteLine(3);
+
+        //reading user input
+        string input = Console.ReadLine();
+        int choise;
+
+        //checking that user input is numeric
+        if (int.TryParse(input, out choise))
+        {
+            switch (choise)
+            {
+                case 1:
+                    Console.WriteLine("you chose the first coffee");
+                    break;
+                    case 2:
+                    Console.WriteLine("you chose the secound coffee");
+                    break ;
+                    case 3:
+                    Console.WriteLine("you chose the third coffee");
+                    break;
+                    default:
+                    Console.WriteLine("please enter a number between 1 and 3");
+                    break;
+            
+            }
+        }
+        else
+        {
+            Console.WriteLine("please enter a valid number");
+        }
+    }
+
+
+
+
+
+}
+
+
+
+
+
